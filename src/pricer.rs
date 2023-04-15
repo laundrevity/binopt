9289ceec177112f0de5b429416e729b38f65a82f
Conor Mahany <mahany25@gmail.com>
2023-04-15 22:39:55 +0200

    adding bench and test for new variant plus hotfixes

--- src/pricer.rs
+++ src/pricer.rs
@@ -1,7 +1,3 @@
-use rayon::prelude::*;
-use std::cmp::max;
-use std::f64;
-
 pub fn call(t: f64, s: f64, k: f64, r: f64, sigma: f64, q: f64, n: usize) -> f64 {
     let delta_t = t / (n as f64);
     let up = (sigma * delta_t.sqrt()).exp();
@@ -16,7 +12,7 @@ pub fn call(t: f64, s: f64, k: f64, r: f64, sigma: f64, q: f64, n: usize) -> f64
 
     for j in (0..n).rev() {
         for i in 0..=j {
-            let binomial_val = p0 * p[i+1] + p1 * p[i];
+            let binomial_val = p0 * p[i + 1] + p1 * p[i];
             let exercise_val = k - s * up.powf(2.0 * (i as f64) - (j as f64));
             p[i] = f64::max(binomial_val, exercise_val);
         }
@@ -25,10 +21,17 @@ pub fn call(t: f64, s: f64, k: f64, r: f64, sigma: f64, q: f64, n: usize) -> f64
     p[0]
 }
 
-
-pub fn binomal_call_price(S: f64, K: f64, ty: f64, r: f64, q: f64, sigma: f64, n: usize) -> Option<f64> {
-    let dt = ty /(n as f64);
-    if dt >= f64::powf(sigma / (r-q), 2.0) {
+pub fn binomial_call_price(
+    S: f64,
+    K: f64,
+    ty: f64,
+    r: f64,
+    q: f64,
+    sigma: f64,
+    n: usize,
+) -> Option<f64> {
+    let dt = ty / (n as f64);
+    if dt >= f64::powf(sigma / (r - q), 2.0) {
         eprintln!("time steps are too large, increase n");
         return None;
     }
@@ -40,7 +43,7 @@ pub fn binomal_call_price(S: f64, K: f64, ty: f64, r: f64, q: f64, sigma: f64, n
     // CRR model
     let up = f64::exp(sigma * f64::sqrt(dt));
     let down = f64::exp(-sigma * f64::sqrt(dt)); // 1/up;
-    let prob = (f64::exp((r-q) * dt) - down) / (up - down);
+    let prob = (f64::exp((r - q) * dt) - down) / (up - down);
 
     let mut s_prices_right: Vec<f64> = vec![0.0; n + 1];
     // maybe actually only store this, s_prices is not really needed
@@ -50,24 +53,22 @@ pub fn binomal_call_price(S: f64, K: f64, ty: f64, r: f64, q: f64, sigma: f64, n
         opt_prices_right[i] = f64::max(s_prices_right[i] - K, 0.0);
     }
 
-
     let mut s_prices_left: Vec<f64> = vec![0.0; s_prices_right.len() - 1];
     // maybe actually only store this, s_prices is not really needed
     let mut opt_prices_left: Vec<f64> = vec![0.0; s_prices_right.len() - 1];
-    for j in (0..=n).rev() {
-        for k in 0..j {
-            s_prices_left[k] = S * up.powf((j - k) as f64) * down.powf(j as f64);
+    for j in (0..n).rev() {
+        for k in 0..=j {
+            s_prices_left[k] = S * up.powf((j - k) as f64) * down.powf(k as f64);
             // s_prices_left[k] = f64::exp(-r * dt) * (prob * s_prices_right[k] + (1.0 - prob) * s_prices_right[k + 1]);
-            opt_prices_left[k] = f64::exp(-r * dt) * (prob * opt_prices_right[k] + (1.0 - prob) * opt_prices_right[k + 1]);
+            opt_prices_left[k] = f64::exp(-r * dt)
+                * (prob * opt_prices_right[k] + (1.0 - prob) * opt_prices_right[k + 1]);
             let exercise_val = f64::max(K - s_prices_left[k], 0.0);
             let opt_price = f64::max(exercise_val, opt_prices_left[k]); // check exercise
             opt_prices_left[k] = opt_price;
         }
-        std::mem::swap(& mut s_prices_left , & mut s_prices_right);
-        std::mem::swap(& mut opt_prices_left , & mut opt_prices_right);
+        std::mem::swap(&mut s_prices_left, &mut s_prices_right);
+        std::mem::swap(&mut opt_prices_left, &mut opt_prices_right);
     }
 
     Some(opt_prices_right[0])
 }
-
-
--- tests/pricer_test.rs
+++ tests/pricer_test.rs
@@ -1,4 +1,4 @@
-use pricer::{call};
+use pricer::{binomial_call_price, call};
 
 const EPSILON: f64 = 1e-6;
 
@@ -20,5 +20,27 @@ fn test_call() {
         "Expected call price: {}, got: {}",
         expected_call_price,
         call_price
+    );
+}
+
+#[test]
+fn test_call_jak() {
+    let s = 100.0;
+    let k = 110.0;
+    let r = 0.05;
+    let t = 1.0;
+    let sigma = 0.25;
+    let q = 0.03;
+    let n = 100;
+
+    let expected_call_price = 21.345507456248857;
+
+    let res = binomial_call_price(s, k, t, r, q, sigma, n);
+
+    assert!(
+        (res.unwrap() - expected_call_price).abs() < EPSILON,
+        "expected call price: {}, got {}",
+        expected_call_price,
+        res.unwrap()
     );
 }
